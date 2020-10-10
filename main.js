b1165375629f4214d501ffd51f91f521e36f626d
Dean Minnie <dminnie33@gmail.com>
2020-10-10 12:58:12 +0200

    Started with the w3 schools tutorial

--- main.js
+++ main.js
@@ -1,66 +1,83 @@
 const Discord = require('discord.js');
 require('dotenv').config();
+var http = require('http');
 
-const client = new Discord.Client()
-
-const prefix = '-';
-
-client.once('ready',() =>{
-    console.log(`Logged in as ${client.user.tag} !`);
-});
-
-client.on('typingStart', typing => {
-});
-
-client.on('message', async message => 
+let Screwie = function()
 {
-    if(!message.content.startsWith(prefix))return;
-    if(message.author.bot)return;
-    if(!message.guild)return;
+    const client = new Discord.Client()
 
-    const args = message.content.split(prefix);
-    //console.log(args);
-    args.shift().toLocaleLowerCase();
-    //console.log(args[0]);
-    const command = args[0];
+    const prefix = '-';
 
-    console.log(command);
+    client.once('ready',() =>{
+        console.log(`Logged in as ${client.user.tag} !`);
+    });
 
-    if(command === 'ping')
+    client.on('typingStart', typing => {
+    });
+
+    client.on('message', async message => 
     {
-        message.channel.send('pong');
-    }else if(command === 'spam')
-    {
-        if (message.member.voice.channel) 
+        if(!message.content.startsWith(prefix))return;
+        if(message.author.bot)return;
+        if(!message.guild)return;
+
+        const args = message.content.split(prefix);
+        //console.log(args);
+        args.shift().toLocaleLowerCase();
+        //console.log(args[0]);
+        const command = args[0];
+
+        console.log(command);
+
+        if(command === 'ping')
         {
-            const connection = await message.member.voice.channel.join();
-            const dispatcher = connection.play('./screem.mp3');
-
-            dispatcher.on('finish', () => 
+            message.channel.send('pong');
+        }else if(command === 'spam')
+        {
+            if (message.member.voice.channel) 
             {
-                message.member.voice.channel.leave();
-            });
-        } else 
-        {
-            message.reply('You need to join a voice channel first!');
-        }
-    }
-    else if(command === 'reee')
-    {
-        if (message.member.voice.channel) 
-        {
-            const connection = await message.member.voice.channel.join();
-            const dispatcher = connection.play('./reee.mp3');
+                const connection = await message.member.voice.channel.join();
+                const dispatcher = connection.play('./screem.mp3');
 
-            dispatcher.on('finish', () => 
+                dispatcher.on('finish', () => 
+                {
+                    message.member.voice.channel.leave();
+                });
+            } else 
             {
-                message.member.voice.channel.leave();
-            });
-        } else 
-        {
-            message.reply('You need to join a voice channel first!');
+                message.reply('You need to join a voice channel first!');
+            }
         }
-    }
-});
+        else if(command === 'reee')
+        {
+            if (message.member.voice.channel) 
+            {
+                const connection = await message.member.voice.channel.join();
+                const dispatcher = connection.play('./reee.mp3');
 
-client.login(process.env.DISCORD_TOKEN);
+                dispatcher.on('finish', () => 
+                {
+                    message.member.voice.channel.leave();
+                });
+            } else 
+            {
+                message.reply('You need to join a voice channel first!');
+            }
+        }
+    });
+
+    client.login(process.env.DISCORD_TOKEN);
+}
+
+let webServer = function()
+{
+    http.createServer(function (req, res) 
+    {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('Hello World!');
+    }).listen(80); 
+}
+
+Screwie();
+webServer();
+console.log('Test');
