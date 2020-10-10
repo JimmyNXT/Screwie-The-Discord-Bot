bd5a89d0b6579c340a837ea96da8c545d95d4825
Dean Minnie <dminnie33@gmail.com>
2020-10-10 09:02:48 +0200

    Added reee

--- main.js
+++ main.js
@@ -7,21 +7,23 @@ const client = new Discord.Client()
 const prefix = '-';
 
 client.once('ready',() =>{
-    console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Logged in as ${client.user.tag} !`);
 });
 
 client.on('typingStart', typing => {
 });
 
-client.on('message', async message => {
+client.on('message', async message => 
+{
     if(!message.content.startsWith(prefix))return;
     if(message.author.bot)return;
+    if(!message.guild)return;
 
     const args = message.content.split(prefix);
     //console.log(args);
-    args.shift();
+    args.shift().toLocaleLowerCase();
     //console.log(args[0]);
-    const command = args[0].toLowerCase();
+    const command = args[0];
 
     console.log(command);
 
@@ -30,14 +32,26 @@ client.on('message', async message => {
         message.channel.send('pong');
     }else if(command === 'spam')
     {
-        //const members = message.author;
-        const spamVoiceCannelID = '764024255994003508';
-
         if (message.member.voice.channel) 
         {
             const connection = await message.member.voice.channel.join();
             const dispatcher = connection.play('./screem.mp3');
-            console.log('Screeming');
+
+            dispatcher.on('finish', () => 
+            {
+                message.member.voice.channel.leave();
+            });
+        } else 
+        {
+            message.reply('You need to join a voice channel first!');
+        }
+    }
+    else if(command === 'reee')
+    {
+        if (message.member.voice.channel) 
+        {
+            const connection = await message.member.voice.channel.join();
+            const dispatcher = connection.play('./reee.mp3');
 
             dispatcher.on('finish', () => 
             {
